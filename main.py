ec10d6a9beee13875ac77590e5c469efc95420db
Mike Cosgrave <mikecosgravevfx@gmail.com>
2025-03-14 14:45:52 +0000

    Initial commit: Slack integration plugin for Dify

---
+++ main.py
@@ -0,0 +1,7 @@
+from dify_plugin import Plugin
+
+def plugin_main(plugin: Plugin) -> None:
+    """Main entry point for the plugin."""
+    # The plugin system will handle loading the endpoints
+    # We don't need to do anything specific here
+    pass
